7203297132871db2d9a510c8084c2fc35ef838d7
Austin <Austin_MacBook@austins-mbp.attlocal.net>
2018-10-16 21:20:25 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  arr.sort_by do |phrase|
+    phrase.split("").map do |char|
+      alphabet.index(char)
+    end
+  end
 end
